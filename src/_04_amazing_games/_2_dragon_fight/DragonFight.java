7275233d3c57e28b768822d9419022ac30a14860
student <student@ip-172-31-52-163.us-west-2.compute.internal>
2021-12-01 19:58:29 -0800

    Dragon Figh

--- src/_04_amazing_games/_2_dragon_fight/DragonFight.java
+++ src/_04_amazing_games/_2_dragon_fight/DragonFight.java
@@ -74,7 +74,7 @@ public class DragonFight {
 			
 				// 11.  Pop up a message that tells us how much health the player and
 				// 		dragon have left.
-
+		JOptionPane.showMessageDialog(null,"Player Health: " + playerHealth + "\nDragon Health: " + dragonHealth);
 			
 			// (Bonus: Also display the amount of health that was lost by each in this
 			// round)
@@ -86,14 +86,14 @@ public class DragonFight {
 
 	static void playerLost() {
 		// 11. Tell the player that they have been defeated by the dragon and have no treasure
-	playerLost =JOptionPane.showMessageDialog(null,"You have been defeated by the dragon. You have no treasure.");
+	int playerLost =JOptionPane.showMessageDialog(null,"You have been defeated by the dragon. You have no treasure.");
 
 		System.exit(0);   //This code ends the program
 	}
 
 	static void dragonLost() {
 		// 12. Tell the user that the dragon has been defeated and they get a ton of gold!
-	dragonLost = JOptionPane.showMessageDialog(null,"You defeated the dragon! You got a ton of gold!");
+	int dragonLost = JOptionPane.showMessageDialog(null,"You defeated the dragon! You got a ton of gold!");
 		System.exit(0);   //This code ends the program
 	}
 
